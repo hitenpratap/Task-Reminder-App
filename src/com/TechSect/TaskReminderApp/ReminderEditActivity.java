a21f4c37e5cccac49d6eafcb95f4e88a760fdcd1
Hiten Pratap Singh <hitenpratap99@gmail.com>
2014-09-28 02:59:09 +0530

    Added ReminderActivityEdit file for TAsk Edit functionality

---
+++ src/com/TechSect/TaskReminderApp/ReminderEditActivity.java
@@ -0,0 +1,16 @@
+package com.TechSect.TaskReminderApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hitenpratap on 28/9/14.
+ */
+public class ReminderEditActivity extends Activity {
+
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.reminder_edit);
+    }
+
+}
