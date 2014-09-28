f3c0ee666cb71eb98369ead8d70fbb04863e0719
Hiten Pratap Singh <hitenpratap99@gmail.com>
2014-09-28 19:19:23 +0530

    Implemented List Activity Class with dummy data for testing all things like layout etc

--- src/com/TechSect/TaskReminderApp/ReminderListActivity.java
+++ src/com/TechSect/TaskReminderApp/ReminderListActivity.java
@@ -3,6 +3,7 @@ package com.TechSect.TaskReminderApp;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class ReminderListActivity extends ListActivity {
     /**
@@ -12,5 +13,8 @@ public class ReminderListActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reminder_list);
+        String[] list = new String[]{"Task Element1","Task Element2","Task Element3","Task Element4","Task Element5"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.reminder_row,R.id.text1,list);
+        setListAdapter(adapter);
     }
 }
