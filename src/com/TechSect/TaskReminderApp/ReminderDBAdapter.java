af163ed33d662119da3c2dc17c191c3b2b11d96d
Hiten Pratap Singh <hitenpratap99@gmail.com>
2014-10-05 02:51:18 +0530

    Added SQLite basic support

---
+++ src/com/TechSect/TaskReminderApp/ReminderDBAdapter.java
@@ -0,0 +1,65 @@
+package com.TechSect.TaskReminderApp;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by hitenpratap on 5/10/14.
+ */
+public class ReminderDBAdapter {
+
+    private static final String DATABASE_NAME = "techSectTaskReminder";
+    private static final String DATABASE_Table = "reminders";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_BODY = "body";
+    public static final String KEY_DATE_TIME = "reminder_date_time";
+    public static final String KEY_ROWID = "_id";
+
+    private DataBaseHelper databaseHelper;
+
+    private SQLiteDatabase database;
+
+    private static final String DATABASE_CREATE = "create table "+DATABASE_Table+" ("+KEY_ROWID+"" +
+            "integer primary key autoincrement, "+KEY_TITLE+" text not null,"
+            +KEY_BODY+" text not null, "+KEY_DATE_TIME+" text not null);";
+
+    private final Context context;
+
+    public ReminderDBAdapter(Context context){
+        this.context = context;
+    }
+
+    public ReminderDBAdapter open() throws SQLException{
+        databaseHelper = new DataBaseHelper(context);
+        database = databaseHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close(){
+        database.close();
+    }
+
+
+    public static class DataBaseHelper extends SQLiteOpenHelper{
+
+        DataBaseHelper(Context context){
+            super(context,DATABASE_NAME,null,DATABASE_VERSION);
+        }
+
+        public void onCreate(SQLiteDatabase database){
+            database.execSQL(DATABASE_CREATE);
+        }
+
+        public void onUpgrade(SQLiteDatabase database,int oldVersion, int newVersion){
+            //////////// used to upgrade the database
+        }
+
+    }
+
+
+}
